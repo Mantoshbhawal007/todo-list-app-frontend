630afa9eaa600abc8a552fd3c42fe5c2ce27fe0e
Mantosh Bhawal <mantoshbhawal@gmail.com>
2023-04-20 16:58:30 +0530

    inital commit

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import { Context } from '../main';
+import Loader from '../components/Loader';
+
+function Profile() {
+  const { isAuthenticated, setIsAuthenticated, loading, setLoading, user } =
+    useContext(Context);
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <div>
+      <h1>{user?.name}</h1>
+      <p>{user?.email}</p>
+    </div>
+  );
+}
+
+export default Profile;
